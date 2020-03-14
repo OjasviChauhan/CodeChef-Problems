4d12826d82b27530ec120854931747df84a2495d
Ojasvi Chauhan <500068394@stu.upes.ac.in>
2020-03-14 14:10:29 +0530

    Create 2. XOR Engine.java

---
+++ March Long Challenge 20/2. XOR Engine.java
@@ -0,0 +1,47 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc = new Scanner(System.in);
+		int T=0;
+		if(sc.hasNextInt())
+		T=sc.nextInt();
+		while(T-- > 0){
+		    int N = sc.nextInt();
+		    int Q = sc.nextInt();
+		    int A[] = new int[N];
+		    for(int i=0;i<N;i++)
+		        A[i] = sc.nextInt();
+		        
+		    for(int i=0;i<Q;i++){
+		        int e=0,o=0;
+		        int P = sc.nextInt();
+		        int B[] = new int[N];
+		        for(int j=0;j<N;j++)
+		            B[j] = (P ^ A[j]);
+		        for(int j=0;j<N;j++){
+		            int count = 0;
+		            count = decToBinary(B[j]);
+		            if(count%2 == 0) e++;
+		            else o++;
+		        }
+		        System.out.println(e+" "+o);
+		    }
+		}
+	}
+	static int decToBinary(int n) { 
+	    int count=0;
+        while (n > 0){ 
+            if(n%2 == 1) count++; 
+            n = n / 2; 
+        } 
+        return count;
+    }
+}
