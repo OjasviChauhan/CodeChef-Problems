acd44e02d06ab55e53a8a050a0d27139fb6550fa
Ojasvi Chauhan <500068394@stu.upes.ac.in>
2020-03-17 01:31:52 +0530

    Update 2. XOR Engine.java

--- March Long Challenge 20/2. XOR Engine.java
+++ March Long Challenge 20/2. XOR Engine.java
@@ -2,10 +2,8 @@ import java.util.*;
 import java.lang.*;
 import java.io.*;
 
-class Codechef
-{
-	public static void main (String[] args) throws java.lang.Exception
-	{
+class Codechef{
+	public static void main (String[] args) throws java.lang.Exception{
 		Scanner sc = new Scanner(System.in);
 		int T=0;
 		if(sc.hasNextInt())
