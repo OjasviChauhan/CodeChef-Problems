682939ee68c50c10aa24d7a2e107811ff71d5bff
Ojasvi Chauhan <500068394@stu.upes.ac.in>
2020-03-17 01:32:46 +0530

    Update 1. Pintu & Fruits.java

--- March Long Challenge 20/1. Pintu & Fruits.java
+++ March Long Challenge 20/1. Pintu & Fruits.java
@@ -1,14 +1,9 @@
-/* package codechef; // don't place package name! */
-
 import java.util.*;
 import java.lang.*;
 import java.io.*;
 
-/* Name of the class has to be "Main" only if the class is public. */
-class Codechef
-{
-	public static void main (String[] args) throws java.lang.Exception
-	{
+class Codechef{
+	public static void main (String[] args) throws java.lang.Exception{
 		Scanner sc = new Scanner(System.in);
 		int T=0;
 		if(sc.hasNextInt())
