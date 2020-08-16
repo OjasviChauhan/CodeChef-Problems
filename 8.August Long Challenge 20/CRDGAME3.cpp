5d554093d04427ffb90a297b403007c6382d44d9
Ojasvi Chauhan <ojasvichauhan2304@gmail.com>
2020-08-16 16:51:03 +0530

    Create CRDGAME3.cpp

---
+++ 8.August Long Challenge 20/CRDGAME3.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define fl(i, a, b) for (long long int i = (a); i < (b); i++)
+#define flr(i, a, b) for(long long int i = (a); i >=(b); i--)
+#define itr(i, A) for(auto i=A.begin(); i != A.end(); ++i)
+#define pb push_back
+#define mp make_pair
+#define endl '\n'
+typedef long long int ll;
+typedef vector<int> VI;
+ll mod1=1000000007;
+ll mod2=67280421310721;
+ll mod3=998244353;
+ll INF=1e18;
+ 
+int main(){
+ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+    ll t;
+    cin>>t;
+    while(t-- > 0)
+    {
+        ll pc,pr;
+        cin>>pc>>pr;
+        
+        double c = ceil(pc/(double)9);
+        double r = ceil(pr/(double)9);
+        
+        if(c < r)
+            cout<<0<<" "<<c<<"\n";
+        else
+            cout<<1<<" "<<r<<"\n";
+    }
+	return 0;
+}
